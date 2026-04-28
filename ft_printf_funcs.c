e3071da26d617c3b7af913e7f6c2d7a9810bec4c
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-04-28 11:52:37 +0100

    Implement character and string output functions

--- ft_printf_funcs.c
+++ ft_printf_funcs.c
@@ -1,1 +1,57 @@
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
+static int	num_size(int n)
+{
+	int		size;
+	long	nbr;
+
+	nbr = n;
+	size = 0;
+	if (nbr == 0)
+		return (1);
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		size++;
+	}
+	while (nbr > 0)
+	{
+		nbr /= 10;
+		size++;
+	}
+	return (size);
+}
+
+int	ft_putstr_fd(char *s, int fd)
+{
+	int	ctr;
+	
+	ctr = 0;
+	if (!s)
+		return (ctr);
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+		ctr++;
+	}
+	return (ctr);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	nbr;
+
+	nbr = n;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		write(fd, "-", 1);
+	}
+	if (nbr > 9)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((nbr % 10) + '0', fd);
+}
