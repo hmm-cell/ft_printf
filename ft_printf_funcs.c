64afe5728c634ddb2b205a9e0b468a0b3fbab805
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-05-05 11:07:26 +0100

    Update ft_printf_funcs.c

--- ft_printf_funcs.c
+++ ft_printf_funcs.c
@@ -49,34 +49,3 @@ int	ft_un_putnbr(unsigned int n)
 	count += ft_putchar(n % 10 + '0');
 	return (count);
 }
-
-int	ft_puthex(unsigned long n, char format)
-{
-	int	count;
-
-	count = 0;
-	if (n >= 16)
-		count += ft_puthex(n / 16, format);
-	if ((n % 16) < 10)
-		count += ft_putchar(n % 16 + '0');
-	else if (format == 'X')
-		count += ft_putchar((n % 16) - 10 + 'A');
-	else if (format == 'x')
-		count += ft_putchar((n % 16) - 10 + 'a');
-	return (count);
-}
-
-int	ft_print_address(unsigned long n)
-{
-	int	count;
-
-	count = 0;
-	if (n == 0)
-		return (ft_putstr("(nil)"));
-	else
-	{
-		count += ft_putstr("0x");
-		count += ft_puthex(n, 'x');
-	}
-	return (count);
-}
